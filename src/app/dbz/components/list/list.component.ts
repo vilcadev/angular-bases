192a3d31baf2a8be6ce6df89ab44137d342b82a0
cesar <vilca@vilcadev.com>
2023-09-08 21:09:14 -0500

    seccion 5 finish

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -12,7 +12,15 @@ export class ListComponent {
   public characterList: Character[] = [{
     name: 'Trunks',
     power: 10
-  }]
+  }];
 
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter(id: string):void{
+    this.onDelete.emit(id);
+  }
 
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,21 +1,32 @@
 import { Component} from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+import { DbzService } from '../services/dbz.service';
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
 export class MainPageComponent {
-  public characters: Character[]=[{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name: 'Goku',
-    power: 9500
-  },{
-    name: 'Vegeta',
-    power: 7500
-  }]
+
+  //Inyección de Dependencias
+  constructor(private dbzService: DbzService){}
+
+  get characters(): Character[]{
+    //Spread o propagación
+    return [...this.dbzService.characters];
+  }
+
+
+  onDeleteCharacter(id:string): void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character){
+    this.dbzService.addCharacter(character);
+  }
 
 }
+
+//!Para los servicios en notas de la presentacion de servicios se dejo una explicacion del ayudante del profe
